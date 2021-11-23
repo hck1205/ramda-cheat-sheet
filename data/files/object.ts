edb319d03bd43ce64ec350043c6040a7e750954a
CkHong <matt.hong@adriel.ai>
2021-11-23 12:26:23 +0900

    added a cheat for updating object value

--- data/files/object.ts
+++ data/files/object.ts
@@ -2,14 +2,25 @@ import { DataType } from './type';
 
 const objectData: DataType[] = [
 	{
-		rawData: `[{"value":"en","label":"English"},{"value":"ko","label":"한국어"}]`,
-		input: `R.compose(
-        R.defaultTo({}),
-        R.find(R.__, rawData),
-        R.propEq('value')
-      )('en')`,
-		output: `{"label": "English", "value": "en"}`,
-		explaination: `R.find(R.propEq('value', lang), LANGS) || {} \nRaw Data 중에 'value'라는 prop이 lang과 같은 값을 반환하고, 없다면 {}를 반환한다.`,
+		rawData: `const rawData = {
+        childObjects: [{
+          id: 'some id',
+          alias: 'some alias'
+        }]
+    }`,
+		input: `const result =  R.over(
+    R.lensProp('childObjects'),
+    R.map(
+        R.when(
+            R.propEq('id', 'some id'),
+            R.assoc('alias', 'updated alias')
+        )
+    ), rawData);`,
+		output: `childObjects: 
+    [{id: "some id", alias: "updated alias"}]`,
+		explaination: `Object 안의 Object List에서 
+    데이터 값을 변경할 Target Object를 찾아 
+    해당 Object의 Alias 값을 변경후 childObjects를 반환`,
 	},
 ];
 
